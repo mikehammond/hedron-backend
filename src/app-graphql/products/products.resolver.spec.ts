cead7b746641b506a1962e1f67160eed0a823500
Michael Hammond <michael.hammond@meltwater.org>
2020-03-02 07:23:50 +0000

    added a couple of providers

--- src/app-graphql/products/products.resolver.spec.ts
+++ src/app-graphql/products/products.resolver.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductsResolver } from './products.resolver';
+import { ProductsService } from './products.service';
+import { IBMDicoveryService } from '../../utils/services/ibm-discovery.service';
+import { ConfigService } from '@nestjs/config';
 
 describe('ProductsResolver', () => {
   let resolver: ProductsResolver;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProductsResolver],
+      providers: [
+        ProductsResolver,
+        ProductsService,
+        IBMDicoveryService,
+        ConfigService
+      ],
     }).compile();
 
     resolver = module.get<ProductsResolver>(ProductsResolver);

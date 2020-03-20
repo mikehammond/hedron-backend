3f656ed79899a819c244dbc7e0121ee94ddd4e95
Michael Hammond <michael.hammond@meltwater.org>
2020-03-20 07:39:24 +0000

    making vendor type nullable

--- src/graphql/vendors/dto/vendor.dto.ts
+++ src/graphql/vendors/dto/vendor.dto.ts
@@ -2,33 +2,33 @@ import { ObjectType, Field, ID } from 'type-graphql';
 
 @ObjectType()
 export class VendorType {
-  @Field(type => ID)
+  @Field(type => ID, { nullable: true })
   readonly _id: string;
 
-  @Field()
+  @Field({ nullable: true })
   readonly userId: string;
 
-  @Field()
+  @Field({ nullable: true })
   readonly name: string;
 
-  @Field()
+  @Field({ nullable: true })
   readonly contact: string;
 
-  @Field()
+  @Field({ nullable: true })
   readonly website: string;
 
-  @Field()
+  @Field({ nullable: true })
   readonly facebook: string;
 
-  @Field()
+  @Field({ nullable: true })
   readonly location: string;
 
-  @Field()
+  @Field({ nullable: true })
   readonly linkedIn: string;
 
-  @Field()
+  @Field({ nullable: true })
   readonly twitter: string;
 
-  @Field()
+  @Field({ nullable: true })
   readonly founded: string;
 }
